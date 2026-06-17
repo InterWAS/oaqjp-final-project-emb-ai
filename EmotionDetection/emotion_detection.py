56cbd53d879bdc33ef7251e9be78c93b1f1249b6
Wendel Silva <interwas@gmail.com>
2026-06-16 20:13:25 -0400

    Package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,39 @@
+"""
+Module with a function to detect emotion
+"""
+import requests 
+import json
+
+def emotion_detector(text_to_analyze):
+    """Function to detect emotion"""
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        ret = {
+            "anger": anger_score,
+            "disgust": disgust_score,
+            "fear": fear_score,
+            "joy": joy_score,
+            "sadness": sadness_score
+        }
+        dominant_emotion = max(ret, key=ret.get)
+        ret["dominant_emotion"] = dominant_emotion
+    else:
+        ret = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    return ret
